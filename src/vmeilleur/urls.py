86ffc7463a02d62f76502da13d4af76ad6cf00e5
BCG <baltrian@hotmail.fr>
2016-12-29 19:49:07 +0100

    1st commit
    1st commit

---
+++ src/vmeilleur/urls.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url, include
+from django.contrib import admin
+
+#from contributions.views import contribuer, consulter, resultat
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    #url(r'^$', contribuer, name="contribuer"),
+    #url(r'^$', name="contribuer"),
+    #url(r'^resultat/(?P<pk>\d+)/$', resultat, name="resultat"),
+    #url(r'^consulter/', consulter, name="consulter"),
+
+    #url(r'^contribuer_json/$', contribuer, name="contribuer_json"),
+]
+
+admin.site.site_header = "Le meilleur"
